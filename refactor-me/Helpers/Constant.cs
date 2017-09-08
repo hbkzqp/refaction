22e49ce7f53af53d8391a5b3999abb0d143cc700
Kevin <kevin.z@debitsuccess.com>
2017-09-09 10:39:07 +1200

    Add Constant

---
+++ refactor-me/Helpers/Constant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+
+namespace refactor_me.Helpers
+{
+    public class Constant
+    {
+        public static readonly string CONNECTION_STRING = ConfigurationManager.ConnectionStrings["ProductContext"].ConnectionString;
+    }
+}
